c2d8fd196d522b51cb0824eb38224492cbaac15a
Fatime62 <suleymanovafatime04@gmail.com>
2026-07-16 03:05:37 +0400

    Youtube trend kodu elave edildi

--- app.py
+++ app.py
@@ -1,1 +1,16 @@
-("YouTube Trend layihəm başladı!"
+from googleapiclient.discovery import build
+from config import API_KEY
+
+youtube = build("youtube", "v3", developerKey=API_KEY)
+
+request = youtube.videos().list(
+    part="snippet",
+    chart="mostPopular",
+    regionCode="AZ",
+    videoCategoryId="10"
+)
+
+response = request.execute()
+
+for video in response["items"]:
+    print(video["snippet"]["title"])
